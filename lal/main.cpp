09e903def17917bca543eaff38e3c64f8ca0bb5b
felipe.celer <felipe.celer@450e779c-4036-0410-a8f3-e78d062b3142>
2008-05-08 18:48:07 +0000

    irei adicionar um arquivo de teste ...

---
+++ lal/main.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <cmath>
+#include <list>
+#include <algorithm>
+
+#include "Point2.hpp"
+#include "Point3.hpp"
+
+#include "Vector2.hpp"
+#include "Vector3.hpp"
+#include "Vector4.hpp"
+
+#include "Matrix3x3.hpp"
+#include "Matrix4x4.hpp"
+
+#include "Quaternion.hpp"
+#include "EigenSystem.hpp"
+#include "Color.hpp"
+#include "BoundingBox3.hpp"
+#include "Math.hpp"
+
+#include <ctime> 
+
+
+int main(int argc, char *argv[])
+{
+
+	std::cout << "AAAAAAAAAAAAA" << std::endl;
+	
+}
+
+
