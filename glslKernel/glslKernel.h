483ce565257639c206fd08c0626d36511d8ec5c6
andmax <andmax@450e779c-4036-0410-a8f3-e78d062b3142>
2009-02-06 23:13:52 +0000

    GLee fixed

--- glslKernel/glslKernel.h
+++ glslKernel/glslKernel.h
@@ -24,7 +24,7 @@
 #ifndef __GLSL__KERNEL__
 #define __GLSL__KERNEL__
 
-#include <GL/GLee.h>
+#include "GLee.h"
 
 /// Tells whether the system support OpenGL SL capabilities
 /// @return true if the system is ready for OpenGL SL
